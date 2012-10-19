35a85f836ad6583aa731a88296d86e7988e993b4
Mikola Lysenko <mikola@bottleneck.(none)>
2012-10-19 00:23:12 -0500

    stuff added

--- test.js
+++ test.js
@@ -5,12 +5,10 @@ var bunny = require('meshdata').bunny;
 //Initialize simulation
 var N = bunny.positions.length;
 var gliders = new Simulator({
-  outer_radius: 0.1
+  outer_radius: 2.0
 });
 
-console.log("Building K...");
 var K = gliders.stiffness_matrix(bunny);
-console.log("Done");
 
 K.state = new Array(N);
 K.next_state = new Array(N);
@@ -18,14 +16,14 @@ for(var i=0; i<N; ++i) {
   K.state[i] = K.next_state[i] = 0.0;
 }
 
+document.mesh = bunny;
 
-//Simulate
-console.log("Simulating...");
-for(var i=0; i<100; ++i) {
+document.next_state = function() {
   gliders.next_state(K);
   
   var tmp = K.state;
   K.state = K.next_state;
   K.next_state = tmp;
+  return K.state;
 }
 
