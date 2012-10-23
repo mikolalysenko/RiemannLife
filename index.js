e6bf4af763caff3608bddd41fd39e73ff139620c
Mikola Lysenko <mikola@bottleneck.(none)>
2012-10-22 23:18:06 -0500

    Added arcball camera code

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var trimesh = require('trimesh');
 var MeshLife = require('./meshlife.js').MeshLife;
 var bunny = require('meshdata').bunny;
 var loop_subdivide = require('./loop_subdivision.js').loop_subdivide;
+var ArcballCamera = require('./arcball.js').ArcballCamera;
+
 
 
 //Flattens an array
@@ -33,7 +35,7 @@ var nextFrame = (function(){
     
 
 //Context and mesh shader variables
-var context, meshShader, simulation, paused = false;
+var context, meshShader, simulation, paused = false, camera = new ArcballCamera();
 
 
 //Initialize game of life
@@ -78,6 +80,12 @@ function rebuild() {
     meshShader.dispose();
   }
   
+  var normals = trimesh.vertex_normals({
+    positions: mesh.positions,
+    faces:     mesh.faces,
+    stars:     simulation.stars
+  });
+  
   //Create mesh shader
   var meshShaderInfo = {    
     vertexShader: [
@@ -86,13 +94,16 @@ function rebuild() {
       "uniform     mat4     cameraProjection;",
       
       "attribute  vec3      position;",
+      "attribute  vec3      normal;",
       "attribute  float     state;",
       
       "varying    float     intensity;",
+      "varying    vec3      f_normal;",
       
       "void main(void) {",
-        "gl_Position = cameraProjection * cameraInverse * transform * vec4( position, 1.0 );",
+        "gl_Position = cameraProjection * cameraInverse * transform * vec4( 0.2*normal*state + position, 1.0 );",
         "intensity = state;",
+        "f_normal = normal;",
       "}"
     ].join("\n"),
     fragmentShader: [
@@ -101,8 +112,11 @@ function rebuild() {
       "#endif",    
 
       "varying float intensity;",
+      "varying vec3  f_normal;",
+      
       "void main() {",
-        "gl_FragColor = vec4(1, intensity, 0, 1);",
+        "float light = 0.3 * dot(normalize(f_normal), vec3(0, 1, 0)) + 0.5;",
+        "gl_FragColor = vec4(vec3(light,light,light) + 0.5 * vec3(intensity*intensity, intensity-0.4, 0), 1);",
       "}"
     ].join("\n"),
     data: {
@@ -110,6 +124,7 @@ function rebuild() {
       cameraInverse:    GLOW.defaultCamera.inverse,
       cameraProjection: GLOW.defaultCamera.projection,
       position:         new Float32Array(flatten(simulation.positions)),
+      normal:           new Float32Array(flatten(normals)),
       state:            simulation.state
     },
     interleave: {
@@ -168,6 +183,14 @@ function init() {
       paused = true;
     }
   });
+  $("#container").mousemove(function(e) {
+    var container = $("#container");
+    camera.update(e.pageX/container.width()-0.5, e.pageY/container.height()-0.5, {
+      rotate: e.which === 1,
+      pan:    e.which === 2,
+      zoom:   e.which === 3
+    });
+  });
   
   //Success
   render();
@@ -177,13 +200,21 @@ function init() {
 function render() {
   //Initialize context
   context.cache.clear();
-  context.enableDepthTest(false);
+  context.enableDepthTest(true);
   context.clear();
   
   //Update game of life
   if(!paused) {
     simulation.step();
   }
+  
+  var matrix = camera.matrix();
+  for(var i=0; i<4; ++i) {
+    for(var j=0; j<4; ++j) {
+      meshShader.transform.value[i+4*j] = matrix[i][j];
+    }
+  }
+  
   meshShader.state.bufferData(simulation.state, GL.DYNAMIC_DRAW);
   meshShader.draw();
   
